eff42cf65492e9d933c9bc03571a8d073823982c
AOHall <juve.andy@gmail.com>
2019-04-06 18:22:50 +0200

    Update contact-form-handler.php

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -6,16 +6,16 @@
 	
 	$email_from = 'juve.andy@gmail.com';
 	$email_subject = "New Form Submission";
-	$email_body = "User Name: $name.\n".
-					"User Email: $visitor_email.\n".
-						"User Message: $message.\n";
+	$email_body = "User Name: $name .\n".
+					"User Email: $visitor_email .\n".
+						"User Message: $message .\n";
 						
 	$to = "hallingstad.andreas@gmail.com";
 	
-	$headers = "From: $email_from \r\n";
+	$headers = "From: $email_from . \r\n";
 	
-	$headers .= "Reply-To: $visitor_email \r\n";
-	mail($to,$email_subject,$email_body,$email_from,$headers, $headers .);
+	$headers .= "Reply-To: $visitor_email . \r\n";
+	mail($to,$email_subject,$email_body,$headers);
 	
 	header("Location: kontakt.html");
 ?> 
